9a0b820653f072b94119563446d147cf10cccb38
namlxuit056 <namlxuit@gmail.com>
2017-01-21 11:49:38 +0700

    change

---
+++ StringConversionFunctions/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	int Integer;
+	char myInt[20];
+	cout << "Enter an integer between 10-100";
+	cin >> myInt;
+	if(atoi(myInt) == 0)
+}
